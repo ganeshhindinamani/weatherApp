5526dae1bed6cff21a61951c369058f181885ab1
Ganesh Hindinamani <71316041+ganeshhindinamani@users.noreply.github.com>
2022-09-08 00:00:49 +0530

    update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
   constructor() {
     this.key = '5KhHPgtqQ90GgVCDCHRklMB8hQzG3rKr';
-    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+    this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
   }
 
   async getCity(city) {
