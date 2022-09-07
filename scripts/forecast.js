3906be40132d248f398363101e4e5a6cefa68750
Ganesh Hindinamani <ganhindinamani@gmail.com>
2022-09-07 23:54:11 +0530

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,32 @@
+class Forecast {
+  constructor() {
+    this.key = '5KhHPgtqQ90GgVCDCHRklMB8hQzG3rKr';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  }
+
+  async getCity(city) {
+    const query = `?apikey=${this.key}&q=${city}`;
+
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+
+    return data[0];
+  }
+
+  async getWeather(id) {
+    const query = `${id}?apikey=${this.key}`;
+
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+
+    return data[0];
+  }
+
+  async updateCity(city) {
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+
+    return { cityDets, weather }
+  }
+}
